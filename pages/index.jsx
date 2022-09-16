6eb2fbfe933581a6e94a949ecc9e2346f356aac3
Lucas Fernandes da Costa <lucas@lucasfcosta.com>
2022-09-17 00:37:26 +0100

    add social media links

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,5 @@
 import "react";
-import { ChakraProvider, Box } from "@chakra-ui/react";
+import { ChakraProvider, Box, Text, Link, Center } from "@chakra-ui/react";
 import Head from "../components/head";
 import Header from "../components/header";
 import IpsumGenerator from "../components/ipsumGenerator";
@@ -12,6 +12,20 @@ export default function Home() {
       <Box pb="36px">
         <IpsumGenerator />
       </Box>
+      <Center>
+        <Text marginBottom="2em">
+          <Link color="teal.500" href="https://twitter.com/thewizardlucas">
+            Twitter
+          </Link>
+          {" — "}
+          <Link
+            color="teal.500"
+            href="https://github.com/lucasfcosta/toguro-ipsum"
+          >
+            Github
+          </Link>
+        </Text>
+      </Center>
     </ChakraProvider>
   );
 }
