19e2ac976568ac7dbd49cecc20a17c76350ee469
Lucas Fernandes da Costa <lucas@lucasfcosta.com>
2022-09-16 19:46:57 +0100

    add base vercel template

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Toguro Ipsum</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </div>
+  )
+}
