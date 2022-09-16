39642d3175a9a083f723228598cf7617c9325fa4
Lucas Fernandes da Costa <lucas@lucasfcosta.com>
2022-09-17 00:38:37 +0100

    add analytics

--- components/head.jsx
+++ components/head.jsx
@@ -26,6 +26,18 @@ const Head = () => {
       <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
       <meta name="msapplication-TileColor" content="#da532c" />
       <meta name="theme-color" content="#ffffff" />
+      <script
+        async
+        defer
+        src="https://scripts.simpleanalyticscdn.com/latest.js"
+      ></script>
+      <noscript>
+        <img
+          src="https://queue.simpleanalyticscdn.com/noscript.gif"
+          alt=""
+          referrerpolicy="no-referrer-when-downgrade"
+        />
+      </noscript>
     </HeadTag>
   );
 };
